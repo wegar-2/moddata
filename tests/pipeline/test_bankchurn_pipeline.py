09c7f5acb24b634a8837f5ae46d6b44cf89efe26
wegar2 <awegrzyn17@gmail.com>
2025-05-29 22:55:27 +0200

    fixed broken unit test for bankchun pipeline

--- tests/pipeline/test_bankchurn_pipeline.py
+++ tests/pipeline/test_bankchurn_pipeline.py
@@ -4,17 +4,20 @@ from moddata.pipeline.bankchurn_pipeline import BankchurnPipeline
 from moddata.src.config import BankchurnPipelineConfig
 
 
-def test_bankchurn_pipeline_run():
+def test_bankchurn_pipeline_tree_like():
     X_train, X_test, y_train, y_test = BankchurnPipeline(
         config=BankchurnPipelineConfig(
             random_state=12345,
-            train_size=0.8
+            train_size=0.8,
+            encoding_and_scaling_model_type="tree_like"
         )
     ).run()
 
-    assert X_train.shape == (8_000, 10)
-    assert X_test.shape == (2_000, 10)
+    assert X_train.shape == (8_000, 11)
+    assert X_test.shape == (2_000, 11)
     assert y_train.shape == (8_000, 1)
     assert y_test.shape == (2_000, 1)
 
     assert np.all(np.array(y_test.index[:3]) == np.array([7867, 1402, 8606]))
+
+
