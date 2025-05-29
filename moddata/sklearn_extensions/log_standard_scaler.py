4c889e8a47127249c3b80aba55f7fe25bb269c44
wegar2 <awegrzyn17@gmail.com>
2025-05-29 23:48:28 +0200

    corrected impleentation of __init__ of the custom log scale transformer that has been inconsistent with SKLearn API

--- moddata/sklearn_extensions/log_standard_scaler.py
+++ moddata/sklearn_extensions/log_standard_scaler.py
@@ -1,6 +1,7 @@
 from typing import Final
 
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.base import BaseEstimator, TransformerMixin
 
@@ -31,9 +32,10 @@ class LogStandardScaler(TransformerMixin, BaseEstimator):
             before log-transformation
             log_base: base of the log-transformation
         """
-        self._shift: Final[float] = shift
-        self._standard_scaler: StandardScaler = StandardScaler()
-        self._log_base: float | int = log_base
+        self.shift: Final[float] = shift
+        self.log_base: float | int = log_base
+
+        self.feature_names_in_ = None
 
     @staticmethod
     def _validate_shift(shift: float) -> None:
@@ -42,16 +44,34 @@ class LogStandardScaler(TransformerMixin, BaseEstimator):
                              f"Received value {shift=}")
 
     def _log_transform(self, X):
-        return np.emath.logn(n=self._log_base, x=X + self._shift)
+        return np.emath.logn(n=self.log_base, x=X + self.shift)
 
-    def fit(self, X, y=None):
+    def fit(self, X: pd.DataFrame):
+        self._standard_scaler: StandardScaler = StandardScaler() # noqa
+        if not isinstance(X, pd.DataFrame):
+            raise ValueError("This estimator only accepts pd.DataFrame input!")
+        self.feature_names_in_ = list(X.columns)
         X_log = self._log_transform(X=X)
         self._standard_scaler.fit(X=X_log)
         return self
 
-    def transform(self, X):
+    def transform(self, X: pd.DataFrame):
+        if not isinstance(X, pd.DataFrame):
+            raise ValueError("This transformer only accepts pd.DataFrame input!")
         X_log = self._log_transform(X=X)
-        return self._standard_scaler.transform(X=X_log + self._shift)
+        return self._standard_scaler.transform(X=X_log + self.shift)
 
     def fit_transform(self, X, y=None, **fit_params):
         return self.fit(X=X).transform(X=X)
+
+    def get_feature_names_out(self, input_features=None):
+        if input_features is None:
+            if self.feature_names_in_ is not None:
+                input_features = self.feature_names_in_
+            else:
+                raise ValueError("No input features provided and none "
+                                 "were stored during fit! ")
+        return [
+            f"log_base_{self.log_base:0.2f}_shift_{self.shift:0.2f}_{feature}"
+            for feature in input_features
+        ]
